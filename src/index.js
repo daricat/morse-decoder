9e2eacb8b7fc0883a47be01199d0cb7759e31cce
daricat <darina.talk@gmail.com>
2020-03-18 23:10:11 +0300

    feat: add solve of task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,61 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = []; 
+  let space = '**********';
+  
+  for ( let i = 0, j = 10; i <= expr.length, j <= expr.length; i += 10, j += 10 ) {    
+    array.push((expr.slice(i, j).split('')));    
+  }
+
+  for ( let k = 0; k < array.length; k++ ) {      
+    for ( let m = 0; m < array[k].length; m++ ) {
+      if ( array[k][m] == '1' ) {
+        array[k].splice(0, m);         
+        break;
+      }      
+    }
+        
+  }
+  
+  for ( let y = 0; y < array.length; y++ ) {
+    for ( let t = 0, z = t + 1; t < array[y].length, t < (array[y].length) - 1; t++, z++ ) {
+      (array[y]).splice(t, 2, (array[y][t]+array[y][t+1]));
+    }
+  }
+
+  for ( let h = 0; h < array.length; h++ ) {
+    for ( let q = 0; q < array[h].length; q++) {
+      switch (array[h][q]) {
+        case '10':
+          array[h][q] = '.';
+          break;
+        case '11':
+          array[h][q] = '-';
+          break;          
+      }
+    }
+  }
+
+  for ( let s = 0; s < array.length; s++ ) {
+    array[s] = array[s].join('');
+  }
+
+  for ( let v = 0; v < array.length; v++ ) {
+    for (var key in MORSE_TABLE) {
+      switch (array[v]) {
+        case key:
+          array[v] = MORSE_TABLE[key];
+          break;
+        case space:
+          array[v] = ' ';
+          break;           
+      }            
+    }
+  }
+  
+
+  return (array.join(''));
 }
 
 module.exports = {
